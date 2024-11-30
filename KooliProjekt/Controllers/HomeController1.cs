fcfdcfcca814de631b88f03b70e03bc8b4801894
Leonid Borodajev <Leonid.Borodajev@tptlive.ee>
2024-11-30 13:29:00 +0200

    ...

---
+++ KooliProjekt/Controllers/HomeController1.cs
@@ -0,0 +1,42 @@
+﻿using KooliProjekt.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using KooliProjekt.Models;
+
+
+namespace KooliProjekt.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
+
+
+
+namespace KooliProjekt.Controllers
+{
+    public class TodoListsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        // Инъекция контекста базы данных через конструктор
+        public TodoListsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Метод Index с поддержкой пагинации
+        public async Task<IActionResult> Index(int page = 1)
+        {
+            // Получение данных с использованием метода расширения GetPaged
+            var data = await _context.TodoLists.GetPaged(page, 5); // 5 элементов на страницу
+            return View(data); // Передача данных в представление
+        }
+    }
+}
+
